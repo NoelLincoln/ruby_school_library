8179585320a89dbe8704cc3c30c7eeb2cd065fe5
Noel Bryant <noelsobryant@gmail.com>
2023-11-22 00:10:55 +0300

    correct constructor parameters, set classroom to be required

--- student.rb
+++ student.rb
@@ -6,9 +6,9 @@ require_relative 'person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(id, age = 18, name = 'Unknown', parent_permission: true, classroom: 'Ruby')
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     # Call the parent's constructor using super
-    super(id, age, name, parent_permission)
+    super(age,name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
