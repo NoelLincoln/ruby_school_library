7acd80fea068f679dd2c3c978116bdcb7c3c20e3
Noel Bryant <noelsobryant@gmail.com>
2023-11-23 12:45:33 +0300

    Create the has-many/belongs-to relationship between Classroom and Student

--- student.rb
+++ student.rb
@@ -1,15 +1,25 @@
 # student.rb
-
 require_relative 'person'
 
 # Represents a student with an additional classroom.
 class Student < Person
-  attr_reader :classroom
+  attr_accessor :classroom
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    # Call the parent's constructor using super
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
+    @classroom = nil
+  end
+
+  def join_classroom(classroom)
     @classroom = classroom
+    classroom.add_student(self)
+  end
+
+  def leave_classroom
+    return unless @classroom
+
+    @classroom.remove_student(self)
+    @classroom = nil
   end
 
   def play_hooky
