91b4ff3d1d1406b69efd5fd7808a1ed19da8faac
Noel Bryant <noelsobryant@gmail.com>
2023-11-23 12:46:40 +0300

    Create a class Book with @title and @author instance variables

---
+++ book.rb
@@ -0,0 +1,17 @@
+# book.rb
+class Book
+  attr_accessor :title, :author, :rentals
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  # Has-many side: a book has many rentals
+  def add_rental(person, date)
+    rental = Rental.new(self, person, date)
+    @rentals << rental
+    person.add_rental(rental)
+  end
+end
