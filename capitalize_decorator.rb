284472ff69b722041a59ded1b09d415087684400
Noel Bryant <noelsobryant@gmail.com>
2023-11-23 07:23:21 +0300

    create capitalize decorator to implement correct_name method from Nameable

---
+++ capitalize_decorator.rb
@@ -0,0 +1,13 @@
+# capitalize_decorator.rb
+require_relative 'nameable'
+
+# Capitalize decorator
+class CapitalizeDecorator < Nameable
+  def initialize(nameable)
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
