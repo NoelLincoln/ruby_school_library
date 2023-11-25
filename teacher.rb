cb57158ae886b98368cd9334e31993cd04e43687
Noel Bryant <noelsobryant@gmail.com>
2023-11-25 20:05:06 +0300

    fix bug ,add attribute acessor for specialization

--- teacher.rb
+++ teacher.rb
@@ -1,16 +1,13 @@
 require_relative 'person'
 
-# Represents a teacher with additional specialization.
 class Teacher < Person
-  attr_reader :specialization
+  attr_accessor :specialization
 
   def initialize(age, specialization, name = 'Unknown', parent_permission: true)
-    # Call the parent's constructor using super
     super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
-  # Override can_use_services? to always return true for teachers
   def can_use_services?
     true
   end
