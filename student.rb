31ddf9aeaf41225533c89e83126456b000af217e
Noel Bryant <noelsobryant@gmail.com>
2023-11-25 20:03:45 +0300

    ensure proper addition of the current object to the students array of the assigned classroom.

--- student.rb
+++ student.rb
@@ -1,28 +1,19 @@
-# student.rb
 require_relative 'person'
-
-# Represents a student with an additional classroom.
 class Student < Person
-  attr_accessor :classroom
+  attr_accessor :name
+  attr_reader :classroom
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
-    @classroom = nil
-  end
-
-  def join_classroom(classroom)
     @classroom = classroom
-    classroom.add_student(self)
-  end
-
-  def leave_classroom
-    return unless @classroom
-
-    @classroom.remove_student(self)
-    @classroom = nil
   end
 
   def play_hooky
-    '¯\\(ツ)/¯'
+    '¯(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.stuedents.push(self) unless classroom.stuedents.include?(self)
   end
 end
