210319793b0a02525ff05b553c124c58226fc948
Noel Bryant <noelsobryant@gmail.com>
2023-11-25 20:05:22 +0300

    ensure proper addition of the current object to the students array of the assigned classroom.

--- student.rb
+++ student.rb
@@ -14,6 +14,6 @@ class Student < Person
 
   def classroom=(classroom)
     @classroom = classroom
-    classroom.stuedents.push(self) unless classroom.stuedents.include?(self)
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
