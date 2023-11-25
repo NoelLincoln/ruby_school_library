0e2f4851bb8cb7637f6b85ddc2a0c5f3306bccc9
Noel Bryant <noelsobryant@gmail.com>
2023-11-25 19:59:24 +0300

    update person object to allow creation of teacher object

--- person.rb
+++ person.rb
@@ -5,13 +5,15 @@ class Person < Nameable
   attr_accessor :name, :age, :rentals
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true, **additional_params)
     super()
     @id = Random.rand(1...1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
     @rentals = []
+
+    @additional_params = additional_params
   end
 
   def can_use_services?
@@ -37,5 +39,24 @@ class Person < Nameable
 
   def of_age?
     @age >= 18
+  end
+end
+
+# student.rb
+class Student < Person
+end
+
+# teacher.rb
+class Teacher < Person
+  attr_reader :specialization
+
+  def initialize(age, name, specialization:, parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
+    @specialization = specialization
+  end
+
+  # Override can_use_services? to always return true for teachers
+  def can_use_services?
+    true
   end
 end
