af0ab1c150e8da495057951b9815e68b2a142f67
Noel Bryant <noelsobryant@gmail.com>
2023-11-25 20:35:30 +0300

    address the Metrics/CyclomaticComplexity issue

--- main.rb
+++ main.rb
@@ -1,48 +1,57 @@
 require_relative 'app'
 
-def main
-  app = App.new
-  puts 'Welcome to School Library App!'
-  puts ' '
+class LibraryApp
+  def initialize
+    @app = App.new
+  end
 
-  loop do
-    display_menu
-    option = user_input
+  def start
+    display_welcome_message
 
-    case option
-    when 1
-      app.all_books
-    when 2
-      app.all_people
-    when 3
-      app.create_person
-    when 4
-      app.create_book
-    when 5
-      app.create_rental
-    when 6
-      app.list_rentals
-    when 7
-      break
-    else
-      puts 'Invalid option. Try again'
+    loop do
+      display_menu
+      option = get_user_input
+
+      case option
+      when 1 then @app.all_books
+      when 2 then @app.all_people
+      when 3 then create_person
+      when 4 then @app.create_book
+      when 5 then @app.create_rental
+      when 6 then @app.list_rentals
+      when 7 then break
+      else
+        puts 'Invalid option. Try again'
+      end
     end
+  end
+
+  private
+
+  def display_welcome_message
+    puts 'Welcome to School Library App!'
+    puts ' '
+  end
+
+  def display_menu
+    puts 'Please select an option by entering a number:'
+    puts '1. List all books'
+    puts '2. List all people'
+    puts '3. Create a person'
+    puts '4. Create a book'
+    puts '5. Create a rental'
+    puts '6. List all rentals for a given person id '
+    puts '7. Exit'
+  end
+
+  def get_user_input
+    gets.chomp.to_i
+  end
+
+  def create_person
+    puts 'Creating a person...'
+    @app.create_person
   end
 end
 
-def display_menu
-  puts 'Please select an option by entering a number:'
-  puts '1. List all books'
-  puts '2. List all people'
-  puts '3. Create a person'
-  puts '4. Create a book'
-  puts '5. Create a rental'
-  puts '6. List all rentals for a given person id '
-  puts '7. Exit'
-end
-
-def user_input
-  gets.chomp.to_i
-end
-
-main
+LibraryApp.new.start
