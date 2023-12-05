4558db90ff9d216b7709bfa1172a07ddb55013bf
Noel Bryant <noelsobryant@gmail.com>
2023-12-05 11:59:12 +0300

    create create_person class

---
+++ create_person_option.rb
@@ -0,0 +1,13 @@
+require_relative 'menu_option'
+
+# create person option
+class CreatePersonOption < MenuOption
+  def execute
+    puts 'Creating a person...'
+    @app.create_person
+  end
+
+  def name
+    'Create a person'
+  end
+end
