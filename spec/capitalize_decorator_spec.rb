8199e827c1a7994304fa1dff0d05777a7efae7b7
Noel Bryant <noelsobryant@gmail.com>
2023-12-08 02:23:12 +0300

    test capitalize decorator

---
+++ spec/capitalize_decorator_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../capitalize_decorator'
+
+RSpec.describe CapitalizeDecorator do
+  let(:nameable) { instance_double('Nameable', correct_name: 'john') }
+  let(:decorator) { CapitalizeDecorator.new(nameable) }
+
+  describe '#correct_name' do
+    it 'capitalizes the correct_name from the wrapped nameable' do
+      expect(decorator.correct_name).to eq('John')
+    end
+  end
+end
