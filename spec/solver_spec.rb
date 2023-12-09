53bb0e8e4097e70c72eee71d4c4ab742a59e9ebf
FilbertKimasa <filbertdan67@gmail.com>
2023-12-09 15:17:31 +0300

    Add tests for the fizzbuzz function

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -16,9 +16,32 @@ describe Solver do
   end
 
   describe '#fizzbuzz' do
-    it 'returns "fizz" for multiples of 3' do
+   it 'returns "fizz" for multiples of 3' do
       solver = Solver.new
       expect(solver.fizzbuzz(3)).to eq('fizz')
+      expect(solver.fizzbuzz(6)).to eq('fizz')
+      expect(solver.fizzbuzz(9)).to eq('fizz')
+    end
+
+    it 'returns "buzz" for multiples of 5' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(5)).to eq('buzz')
+      expect(solver.fizzbuzz(10)).to eq('buzz')
+      expect(solver.fizzbuzz(20)).to eq('buzz')
+    end
+
+    it 'returns "fizzbuzz" for multiples of both 3 and 5' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+      expect(solver.fizzbuzz(30)).to eq('fizzbuzz')
+      expect(solver.fizzbuzz(45)).to eq('fizzbuzz')
+    end
+
+    it 'returns the input as a string for other cases' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(7)).to eq('7')
+      expect(solver.fizzbuzz(14)).to eq('14')
+      expect(solver.fizzbuzz(22)).to eq('22')
     end
   end
 end
