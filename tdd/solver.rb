bf59b9cdf510975e0d84b99b01d8918a6893ac11
FilbertKimasa <filbertdan67@gmail.com>
2023-12-09 15:27:14 +0300

    Add fizzbuzz function

--- tdd/solver.rb
+++ tdd/solver.rb
@@ -12,6 +12,18 @@ class Solver
     (1..num).reduce(:*)
   end
 
+  def fizzbuzz(n)
+    if n % 3 == 0 && n % 5 == 0
+      'fizzbuzz'
+    elsif n % 3 == 0
+      'fizz'
+    elsif n % 5 == 0
+      'buzz'
+    else
+      n.to_s
+    end
+  end
+
   private
 
   def valid_input?(num)
